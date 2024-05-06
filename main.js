6afe94e5b41fef2a27fe62734a3306345966e114
ayulstri <ayulstri175@gmail.com>
2024-05-06 09:44:36 +0700

    menampilkan

--- main.js
+++ main.js
@@ -20,3 +20,64 @@ const firebaseConfig = {
   messagingSenderId: "579109661574",
   appId: "1:579109661574:web:4a7cd4060f70eded945a07"
 };
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarPenjual() {
+  const refDokumen = collection(db, "penjual");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      alamat: dok.data().alamat,
+      gmail: dok.data().gmail,
+      noTlpn: dok.data().noTlpn,
+    });
+  });
+  
+  return hasil;
+}
+
+export function formatAngka(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export async function  tambahPenjual(nama, alamat, gmail, noTlpn) {
+  try {
+    const dokRef = await addDoc(collection(db, 'penjual'), {
+      nama: nama,
+      alamat: alamat,
+      gmail: gmail,
+      noTlpn: noTlpn
+    });
+    console.log('Berhasil menambah penjual' + dokRef.id);
+  } catch (e) {
+    console.log('Gagal menambah penjual' + e);
+  }
+   }
+   
+   export async function hapusPenjual(docId) {
+  await deleteDoc(doc(db, "penjual", docId));
+}
+
+export async function ubahPenjual(docId, nama, alamat, gmail, noTlpn) {
+  await updateDoc(doc(db, "penjual", docId), {
+    nama: nama,
+    alamat: alamat,
+    gmail: gmail,
+    noTlpn: noTlpn
+  });
+}
+
+export async function ambilPenjual(docId) {
+  const docRef = await doc(db, "penjual", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data();
+}
